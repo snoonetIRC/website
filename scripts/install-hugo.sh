9bf4ef538992f3b64f5d888f5d0aaabafe2decf4
Zach Bloomquist <zbloomq@live.com>
2019-01-19 12:32:47 -0500

    Treat Hugo the way Hugo wants to be treated

---
+++ scripts/install-hugo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export $LAST=`pwd`
+echo "Installing Hugo Extended Edition ^TM"
+mkdir $HOME/src
+cd $HOME/src
+git clone https://github.com/gohugoio/hugo.git
+cd hugo
+go install --tags extended
+echo "Hugo installation complete. Returning to $LAST"
+cd $LAST
