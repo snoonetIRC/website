f8ab0f18637070dbf90754d900c33ee051127ac4
Katrina Swales <kat.swales@nekokittygames.com>
2019-01-29 23:20:09 +0000

    Fixed #33 by adding support for mobile styles.

--- themes/Snoonet/static/js/application.js
+++ themes/Snoonet/static/js/application.js
@@ -1,55 +1,71 @@
 $(document).ready(function() {
+    if($(window).width()<500)
+    {
+        if ($('#menutoggle').hasClass('open')) {
+            $('#menutoggle').removeClass('open');
+            closeMenu();
+        }
+    }
+
+    function closeMenu() {
+        $($('#sidebar').find('a').get().reverse()).each(function (i) {
+            var _this = this;
+
+            setTimeout(function () {
+                $(_this).animate({
+                    opacity: 0,
+                    // left: -20
+                }, 400, function () {
+                    $(_this).hide();
+
+                    $(_this).css({
+                        left: -20
+                    });
+
+                    if (i >= $('#sidebar').find('a').length - 1) {
+                        if ($('body').hasClass('webchat')) {
+                            $('#main').animate({
+                                marginLeft: 100,
+                                marginRight: 100
+                            }, 400, function () {
+                                $('#content').animate({
+                                    maxWidth: 3000
+                                }, 800, function () {
+                                });
+                            });
+                        }
+                    }
+                });
+            }, i * 30);
+        });
+    }
+
+    function openMenu() {
+        if ($('body').hasClass('webchat')) {
+            $('#content').animate({
+                maxWidth: 1360
+            }, 800, function () {
+                $('#main').animate({
+                    marginLeft: 220,
+                    marginRight: 220
+                }, 400, function () {
+                    open();
+                });
+            });
+        } else {
+            open();
+        }
+    }
+
     $(document).on('click', '#menutoggle', function() {
         if ($(this).hasClass('open')) {
             $(this).removeClass('open');
 
-            $($('#sidebar').find('a').get().reverse()).each(function(i) {
-                var _this = this;
-
-                setTimeout(function() {
-                    $(_this).animate({
-                        opacity: 0,
-                        // left: -20
-                    }, 400, function() {
-                        $(_this).hide();
-
-                        $(_this).css({
-                            left: -20
-                        });
-
-                        if (i >= $('#sidebar').find('a').length - 1) {
-                            if ($('body').hasClass('webchat')) {
-                                $('#main').animate({
-                                    marginLeft: 100,
-                                    marginRight: 100
-                                }, 400, function() {
-                                    $('#content').animate({
-                                        maxWidth: 3000
-                                    }, 800, function() {
-                                    });
-                                });
-                            }
-                        }
-                    });
-                }, i * 30);
-            });
+            closeMenu();
         } else {
             $(this).addClass('open');
 
-            if ($('body').hasClass('webchat')) {
-                $('#content').animate({
-                    maxWidth: 1360
-                }, 800, function() {
-                    $('#main').animate({
-                        marginLeft: 220,
-                        marginRight: 220
-                    }, 400, function() {
-                        open();
-                    });
-                });
-            } else {
-                open();
-            }
+            openMenu();
         }
     });
 });
