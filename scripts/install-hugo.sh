405b572e8e19b5150b1ee14453a391090f1f5a77
linuxdaemon <linuxdaemon@snoonet.org>
2019-04-12 22:49:20 -0500

    Pin hugo version for builds
    Breaking changes may be introduced to the hugo master branch, and break
    our site

--- scripts/install-hugo.sh
+++ scripts/install-hugo.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 SRC_DIR="${HOME}/src"
+HUGO_VERSION="v0.54.0"
 
 echo "Installing Hugo Extended Edition ^TM"
 mkdir -p $SRC_DIR
 pushd $SRC_DIR
-git clone https://github.com/gohugoio/hugo.git
+git clone -b $HUGO_VERSION https://github.com/gohugoio/hugo.git
 cd hugo
 go install --tags extended
 echo "Hugo installation complete."
